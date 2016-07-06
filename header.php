c627805a3f12feb35b62fd5b83bb0b419e2a319b
steffen <st3ff3n.l@gmail.com>
2016-07-06 19:19:59 +0200

    da

---
+++ header.php
@@ -0,0 +1,1 @@
+<h1>hahdahdhda</h1>
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Steffen</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="node_modules/bootstrap/less/bootstrap.less" rel="stylesheet">
+    
+    <!-- Custom LESS -->
+    <link href="src/less/all.css" rel="stylesheet">
+    
+
+</head>
+
+<body>
+
+<?php include ("header.php"); ?>
+
+
+
+
+
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
